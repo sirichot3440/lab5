06d91a62262c7373e03a95d74c53a19ac1498cc9
Sirichot B. <sirichot.b@ku.th>
2025-09-05 11:51:52 +0700

    initial commit

--- delete_course.php
+++
@@ -1,9 +0,0 @@
-<?php
-require("connect_db.php");
-$course_code = $_GET["course_code"];
-
-$sql ="DELETE FROM courses WHERE course_code='$course_code'";
-mysqli_query($conn, $sql);
-header("location: course_list.php");
-exit(0);
-?>
